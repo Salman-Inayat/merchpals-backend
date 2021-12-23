74b084a256a1ff8d28c59c001faf35f546503b40
Imran Naqvi <imran.naqvi@innovage.io>
2021-12-23 12:32:22 +0500

    mp3-14 ImranNaqvi registration

--- controller/authController.js
+++ controller/authController.js
@@ -1,26 +1,53 @@
-const twilioClient = require("../config/twilio");
-const AppError = require("../utils/appError")
-const { catchAsync } = require("./errorController");
+const twilioClient = require('../config/twilio');
+const AppError = require('../utils/appError');
+const { catchAsync } = require('./errorController');
+const User = require('../models/user');
 
-  exports.userSignup = catchAsync(async (req, res, next) => {
-    console.log('req.body', req.body);
-    const verification = await twilioOtpService(req.body.phoneNo);
-    console.log('verification',verification);
-    res.status(200).json({
-      message: "SignUp Successful",
+const twilioOtpService = async (phoneNo) => {
+  try {
+    const response = await twilioClient.verify
+      .services(process.env.TWILIO_MERCHPALS_VERIFICATION_SERVICE)
+      .verifications.create({
+        to: phoneNo,
+        channel: 'sms',
+      });
+
+    return response;
+  } catch (error) {
+    throw new Error();
+  }
+};
+
+exports.userSignup = async (req, res) => {
+  try {
+    const user = await User.createUser(req.body.data);
+    await twilioOtpService(req.body.data.phoneNo);
+
+    return res.status(200).json({
+      user,
+      message: 'SignUp Successful',
     });
-  });
+  } catch (error) {
+    if (error.name === 'object') {
+      return res.status(400).send(error);
+    }
 
-  exports.createVerificationService = catchAsync(async (req, res, next) => {
+    return res.status(400).send({ message: error.message });
+  }
+};
+
+// TODO: We might do not need this function
+// Delete after discussion if needed because does not even returns any value
+exports.createVerificationService = catchAsync(async () => {
   // Create a verification service
-  const service = await twilioClient.verify.services.create({
-    friendlyName: "Merchpals",
+  await twilioClient.verify.services.create({
+    friendlyName: 'Merchpals',
     codeLength: 4,
   });
 });
 
 exports.sendOTP = catchAsync(async (req, res, next) => {
-  const phoneNo = req.body.phoneNo;
+  const { phoneNo } = req.body;
   try {
     const verification = await twilioOtpService(phoneNo);
     return res.status(200).json({
@@ -31,34 +58,20 @@ exports.sendOTP = catchAsync(async (req, res, next) => {
   }
 });
 
-const twilioOtpService = async (phoneNo) => {
+exports.verifyOTP = catchAsync(async (req, res) => {
   try {
-    const response = await twilioClient.verify
-    .services(process.env.TWILIO_MERCHPALS_VERIFICATION_SERVICE)
-    .verifications.create({
-      to: phoneNo,
-      channel: "sms",
-    }); 
-
-    return response;
-  } catch (error) {
-    throw new Error
-  }
-};
-
-exports.verifyOTP = catchAsync(async (req, res, next) => {
-  try {
-    const verificationCheck = await twilioClient.verify
+    const otp = await twilioClient.verify
       .services(process.env.TWILIO_MERCHPALS_VERIFICATION_SERVICE)
       .verificationChecks.create({ to: req.body.phoneNo, code: req.body.code });
-      console.log('verificationCheck', verificationCheck);
-    res.status(200).json({
-      verificationCheck,
-    });
+
+    if (!otp.valid) {
+      throw new Error('Invalid OTP!');
+    }
+
+    const user = await User.updatePhoneVerification(req.body.phoneNo);
+    res.status(200).json({ user });
   } catch (err) {
-    res.status(err.status).json({
-        message: "Verification not found"
-    })
+    console.log('verifyOTP func', err.status, err.message);
+    res.status(500).json({ message: err.message });
   }
 });
- 
