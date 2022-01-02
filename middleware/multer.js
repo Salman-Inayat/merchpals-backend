d0a1c76ecbaf94efdab2a6c64ca63cacaa2c38b1
Ayyaz Ali <ayyazali@Ayyazs-MacBook-Pro.local>
2022-01-02 23:37:31 +0500

    multer file upload

--- middleware/multer.js
+++ middleware/multer.js
@@ -3,55 +3,54 @@ const aws = require('aws-sdk');
 const BUCKET_NAME = 'merchpals-mvp';
 
 var multerS3 = require('multer-s3');
-// aws.config.region = 'us-east-2';
+aws.config.region = 'us-east-2';
 
 // aws.config.update({
 //   accessKeyId: 'AKIAWUDRFMMZSL2ATNGU',
-// secretAccessKey: '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',
-// region: 'us-east-2'
-// })
+//   secretAccessKey: 'gVr6A976ziFZhfXDFH1kYIche731om7UcQPwDrMY',
+//   region: 'us-east-2',
+// });
 // var upload = multer({
 //   storage: multerS3({
 //     s3: new aws.S3(),
 //     bucket: BUCKET_NAME,
 //     acl: 'public-read',
 //     contentType: multerS3.AUTO_CONTENT_TYPE,
-//     metadata: function(req, file, cb) {
+//     metadata: function (req, file, cb) {
 //       console.log(file);
-//       cb(null, { fieldName: file.fieldname });
+//       cb(null, 'test.jpg');
 //     },
-//     key: function(req, file, cb) {
+//     key: function (req, file, cb) {
 //       cb(null, '/' + Date.now().toString() + '_' + file.originalname);
 //     },
 //   }),
 // });
 
-
-const PATH = './uploads/';
+const PATH = './public/';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, PATH);
     },
     filename: (req, file, cb) => {
-        const fileName = 'req.body.name';
+        const fileName = 'test.jpg';
         cb(null, fileName)
     }
 });
 
-
 const upload = multer({
     storage: storage,
     fileFilter: (req, file, cb) => {
-        if (file.mimetype == "application/pdf") {
+
+        console.log(file)
+        // if (file.mimetype == "application/pdf") {
             cb(null, true);
-        } else {
-            cb(null, false);
-            return cb(new Error('Allowed only .pdf'));
-        }
+        // } else {
+            // cb(null, false);
+            // return cb(new Error('Allowed only .pdf'));
+        // }
     }
 });
-
 
 module.exports = {
   upload,
