5398fe3e028c3e456137e69145efad3e12ca67ab
rehmanaliinnovage <rehman.ali@innovage.io>
2022-02-08 23:21:33 +0500

    email template send successfully

--- controller/order.js
+++ controller/order.js
@@ -4,6 +4,46 @@ const Customer = require('../models/customer');
 const Payment = require('../models/payment');
 const sendEmail = require('../utils/email');
 const { DELETED } = require('../constants/statuses');
+const design = require('../models/design');
+const { productsSlug } = require('../constants/productMappings');
+
+const SendOrderEmail = async (orderId, req) => {
+  const data = await Order.getOrderById(orderId);
+
+  let product = [],
+    totalProducts = data.price,
+    totalAmount = data.totalAmount;
+
+  data.products.forEach(productitem => {
+    product.push({
+      productImg: productitem.vendorProduct.productId.image,
+      designImg: productitem.vendorProduct.designId.url,
+      productQuantity: productitem.quantity,
+      productColor: productitem.productMapping.color.label,
+      productName: productitem.vendorProduct.productId.name,
+      productSlug: productitem.vendorProduct.productId.slug,
+      productTotalAmount: (productitem.quantity * productitem.vendorProduct.price).toFixed(2),
+      productSize: productitem.productMapping.variant.label,
+    });
+  });
+  //TODO: we need to change orderId direction when we fix the orderId place
+
+  const replacements = {
+    orderId: data.printfulOrderMetadata.id,
+    customerFirstName: data.customer.firstName,
+    customerLastName: data.customer.lastName,
+    address: data.billingAddress.street,
+    orderDate: data.createdAt.slice(0, 10).replace(/-/g, '/'),
+    totalProducts: totalProducts,
+    orderCost: data.tax,
+    totalShipping: data.shippingCost,
+    totalOrder: totalAmount,
+    products: product,
+    tickImg: req.get('origin') + '/assets/img/tick.png',
+    faqUrl: req.get('origin') + '/faq',
+  };
+  return replacements;
+};
 
 const createOrder = async (req, res) => {
   const orderId = mongoose.Types.ObjectId();
@@ -19,13 +59,15 @@ const createOrder = async (req, res) => {
       req.body.printfulData,
       req.body.storeUrl,
     );
-
     await Payment.createAndChargeCustomer(req.body.payment, order, recordId, req.body.printfulData);
-
+    const data = await SendOrderEmail(order._id, req);
+    console.log('function response', data);
     await sendEmail({
       email: req.body.customer.email,
       subject: 'order sent',
-      template: 'createOrder',
+      template: 'orderCreate',
+      replacements: data,
+      // text: 'rehman ali text',
     });
 
     res.status(200).json({ order, message: 'Order created successfully' });
