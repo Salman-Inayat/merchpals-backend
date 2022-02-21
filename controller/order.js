55710b002f612ae65611c34545a7960068c38426
rehmanaliinnovage <rehman.ali@innovage.io>
2022-02-21 20:09:06 +0500

    change  order number place

--- controller/order.js
+++ controller/order.js
@@ -15,8 +15,6 @@ const SendOrderEmail = async (orderId, req) => {
   let product = [],
     totalProducts = data.price,
     totalAmount = data.totalAmount;
-
-  console.log('order fetched data', data);
   data.products.forEach(productitem => {
     product.push({
       productImg: productitem.vendorProduct.productId.image,
@@ -29,10 +27,9 @@ const SendOrderEmail = async (orderId, req) => {
       productSize: productitem.productMapping.variant.label,
     });
   });
-  //TODO: we need to change orderId direction when we fix the orderId place
 
   const replacements = {
-    orderId: data.printfulOrderMetadata.id,
+    orderId: data.orderNo,
     customerFirstName: data.customer.firstName,
     customerLastName: data.customer.lastName,
     address: data.billingAddress.street,
@@ -46,8 +43,7 @@ const SendOrderEmail = async (orderId, req) => {
     faqUrl: req.get('origin') + '/faq',
   };
 
-  // console.log('replacements', replacements);
-  return data;
+  return replacements;
 };
 
 const createOrder = async (req, res) => {
@@ -64,7 +60,6 @@ const createOrder = async (req, res) => {
       req.body.printfulData,
       req.body.storeUrl,
     );
-    console.log('order', order);
     await Payment.createAndChargeCustomer(req.body.payment, order, recordId, req.body.printfulData);
     const data = await SendOrderEmail(order._id, req);
 
@@ -81,7 +76,6 @@ const createOrder = async (req, res) => {
     await Order.findByIdAndUpdate(orderId, { $set: { status: DELETED } });
     await Payment.findByIdAndRemove(paymentId);
 
-    console.log('create order controller', error);
     res.status(400).json({ message: error.message });
   }
 };
@@ -89,14 +83,14 @@ const createOrder = async (req, res) => {
 const trackOrder = async (req, res) => {
   try {
     const order = await Order.findOne({
-      orderNo: req.body.orderNo,
+      'printfulOrderMetadata.id': req.body.orderNo,
     });
     if (!order) {
       throw new Error('Order not found');
     }
 
     const orderNo = req.body.orderNo;
-    const printfulOrderId = orderNo.slice(3);
+    const printfulOrderId = parseInt(orderNo.slice(3));
 
     const response = await axios.get(`${PRINTFUL_API}/orders/${printfulOrderId}`, {
       headers: {
@@ -114,7 +108,6 @@ const trackOrder = async (req, res) => {
 
     res.status(200).json({ data, message: 'Order tracked successfully' });
   } catch (error) {
-    console.log('track order controller', error);
     res.status(400).json({ message: error.message });
   }
 };
