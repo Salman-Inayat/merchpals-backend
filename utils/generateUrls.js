510f3773ac0f1cbc13352d5e45b92753c4c848fe
Salman-Inayat <salmaninayat1999@gmail.com>
2022-03-03 13:28:56 +0500

    Canvas modes bugs fixed

--- utils/generateUrls.js
+++ utils/generateUrls.js
@@ -1,133 +1,133 @@
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 const aws = require('aws-sdk');
 
 aws.config.update({
-  accessKeyId: process.env.AWS_MP_SYSTEM_ACCESS_ID,
-  secretAccessKey: process.env.AWS_MP_SYSTEM_SECRET_KEY,
-  region: process.env.AWS_S3_REGION,
+    accessKeyId: process.env.AWS_MP_SYSTEM_ACCESS_ID,
+    secretAccessKey: process.env.AWS_MP_SYSTEM_SECRET_KEY,
+    region: process.env.AWS_S3_REGION,
 });
 
 const generatePresignedURLs = () => {
-  const s3 = new aws.S3({
-    accessKeyId: process.env.AWS_MP_SYSTEM_ACCESS_ID,
-    secretAccessKey: process.env.AWS_MP_SYSTEM_SECRET_KEY,
-    region: process.env.AWS_S3_REGION,
-    Bucket: process.env.AWS_S3_DESIGN_BUCKET,
-    signatureVersion: 'v4',
-  });
+    const s3 = new aws.S3({
+        accessKeyId: process.env.AWS_MP_SYSTEM_ACCESS_ID,
+        secretAccessKey: process.env.AWS_MP_SYSTEM_SECRET_KEY,
+        region: process.env.AWS_S3_REGION,
+        Bucket: process.env.AWS_S3_DESIGN_BUCKET,
+        signatureVersion: 'v4',
+    });
 
-  const id = uuidv4();
+    const id = uuidv4();
 
-  const urlNames = [
-    'logo.png',
-    'cover-avatar.png',
-    'front-3600x3600.png',
-    'front-2700x2700.png',
-    'front-1050x1050.png',
-    'front-879x1833.png',
-    'front-thumbnail.png',
-    'front-design.json',
-    'back-2700x2700.png',
-    'back-thumbnail.png',
-    'back-design.json',
-  ];
+    const urlNames = [
+        'logo.png',
+        'cover-avatar.png',
+        'front-3600x3600.png',
+        'front-2700x2700.png',
+        'front-1050x1050.png',
+        'front-879x1833.png',
+        'front-thumbnail.png',
+        'front-design.json',
+        'back-2700x2700.png',
+        'back-thumbnail.png',
+        'back-design.json',
+    ];
 
-  const getUrls = urlNames.map(name => {
-    const params = {
-      Bucket: process.env.AWS_S3_DESIGN_BUCKET,
-      Key: `${id}/${name}`,
+    const getUrls = urlNames.map(name => {
+        const params = {
+            Bucket: process.env.AWS_S3_DESIGN_BUCKET,
+            Key: `${id}/${name}`,
+        };
+
+        const URL = `https://${process.env.AWS_S3_DESIGN_BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${id}/${name}`;
+        return {
+            name,
+            imageUrl: URL,
+        };
+    });
+
+    const putUrls = urlNames.map(name => {
+        const params = {
+            Bucket: process.env.AWS_S3_DESIGN_BUCKET,
+            Key: `${id}/${name}`,
+            Expires: 60 * 5,
+            ContentType: name.includes('.json') ? 'application/json' : 'image/png',
+        };
+
+        const URL = s3.getSignedUrl('putObject', params);
+        return {
+            name,
+            imageUrl: URL,
+        };
+    });
+
+    const urls = {
+        putUrls,
+        getUrls,
     };
 
-    const URL = `https://${process.env.AWS_S3_DESIGN_BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${id}/${name}`;
-    return {
-      name,
-      imageUrl: URL,
-    };
-  });
-
-  const putUrls = urlNames.map(name => {
-    const params = {
-      Bucket: process.env.AWS_S3_DESIGN_BUCKET,
-      Key: `${id}/${name}`,
-      Expires: 60 * 5,
-      ContentType: 'image/png',
-    };
-
-    const URL = s3.getSignedUrl('putObject', params);
-    return {
-      name,
-      imageUrl: URL,
-    };
-  });
-
-  const urls = {
-    putUrls,
-    getUrls,
-  };
-
-  return urls;
+    return urls;
 };
 
 const generateDesignPresignedURLs = () => {
-  const s3 = new aws.S3({
-    accessKeyId: process.env.AWS_MP_SYSTEM_ACCESS_ID,
-    secretAccessKey: process.env.AWS_MP_SYSTEM_SECRET_KEY,
-    region: process.env.AWS_S3_REGION,
-    Bucket: process.env.AWS_S3_DESIGN_BUCKET,
-    signatureVersion: 'v4',
-  });
+    const s3 = new aws.S3({
+        accessKeyId: process.env.AWS_MP_SYSTEM_ACCESS_ID,
+        secretAccessKey: process.env.AWS_MP_SYSTEM_SECRET_KEY,
+        region: process.env.AWS_S3_REGION,
+        Bucket: process.env.AWS_S3_DESIGN_BUCKET,
+        signatureVersion: 'v4',
+    });
 
-  const id = uuidv4();
+    const id = uuidv4();
 
-  const urlNames = [
-    'front-3600x3600.png',
-    'front-2700x2700.png',
-    'front-1050x1050.png',
-    'front-879x1833.png',
-    'front-thumbnail.png',
-    'front-design.json',
-    'back-2700x2700.png',
-    'back-thumbnail.png',
-    'back-design.json',
-  ];
+    const urlNames = [
+        'front-3600x3600.png',
+        'front-2700x2700.png',
+        'front-1050x1050.png',
+        'front-879x1833.png',
+        'front-thumbnail.png',
+        'front-design.json',
+        'back-2700x2700.png',
+        'back-thumbnail.png',
+        'back-design.json',
+    ];
 
-  const getUrls = urlNames.map(name => {
-    const params = {
-      Bucket: process.env.AWS_S3_DESIGN_BUCKET,
-      Key: `${id}/${name}`,
+    const getUrls = urlNames.map(name => {
+        const params = {
+            Bucket: process.env.AWS_S3_DESIGN_BUCKET,
+            Key: `${id}/${name}`,
+        };
+
+        const URL = `https://${process.env.AWS_S3_DESIGN_BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${id}/${name}`;
+        return {
+            name,
+            imageUrl: URL,
+        };
+    });
+
+    const putUrls = urlNames.map(name => {
+        const params = {
+            Bucket: process.env.AWS_S3_DESIGN_BUCKET,
+            Key: `${id}/${name}`,
+            Expires: 60 * 5,
+            ContentType: name.includes('.json') ? 'application/json' : 'image/png',
+        };
+
+        const URL = s3.getSignedUrl('putObject', params);
+        return {
+            name,
+            imageUrl: URL,
+        };
+    });
+
+    const urls = {
+        putUrls,
+        getUrls,
     };
 
-    const URL = `https://${process.env.AWS_S3_DESIGN_BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${id}/${name}`;
-    return {
-      name,
-      imageUrl: URL,
-    };
-  });
-
-  const putUrls = urlNames.map(name => {
-    const params = {
-      Bucket: process.env.AWS_S3_DESIGN_BUCKET,
-      Key: `${id}/${name}`,
-      Expires: 60 * 5,
-      ContentType: 'image/png',
-    };
-
-    const URL = s3.getSignedUrl('putObject', params);
-    return {
-      name,
-      imageUrl: URL,
-    };
-  });
-
-  const urls = {
-    putUrls,
-    getUrls,
-  };
-
-  return urls;
+    return urls;
 };
 
 module.exports = {
-  generatePresignedURLs,
-  generateDesignPresignedURLs,
+    generatePresignedURLs,
+    generateDesignPresignedURLs,
 };
